b79f610bc7a9ba2c861c8a9629582ef13250990d
Henrik Hautajärvi <henrik.hautajarvi@gmail.com>
2021-05-07 19:28:47 +0300

    improved styling and layouts

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from os import getenv
+import locale
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+locale.setlocale(locale.LC_TIME, 'fi_FI.UTF-8')
 
 import routes
